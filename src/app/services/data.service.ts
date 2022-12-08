9612fc53b46a548006b3d3fce49c5943eb0fdca7
Bor9 <Losenkov15@gmail.com>
2022-12-09 02:08:59 +0300

    updt

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,6 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { forkJoin, Observable } from 'rxjs';
-import test_data_layer0 from '../../assets/test_data_layer0.json';
-import test_data_layer1 from '../../assets/test_data_layer1.json';
 import { IData, IDataFull } from '../types/types';
 
 @Injectable({
@@ -11,12 +9,6 @@ import { IData, IDataFull } from '../types/types';
 export class DataService {
   constructor(private http: HttpClient) {}
 
-  getFullArray(): Observable<any> {
-    const data0 = this.http.get('assets/test_data_layer0.json');
-    const data1 = this.http.get('assets/test_data_layer1.json');
-
-    return forkJoin([data0, data1]);
-  }
   getFirstArray(): Observable<any> {
     const data0 = this.http.get('http://193.33.194.100:8080/general');
     return data0;
@@ -37,9 +29,5 @@ export class DataService {
       });
       return data0;
     }
-  }
-  getSecLol(): Observable<any> {
-    const data0 = this.http.get('http://193.33.194.100:8080/detail');
-    return data0;
   }
 }
